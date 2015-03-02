2898512fb98543007b430941dc0347b91262d084
dinglevin <dingweiqiong@gmail.com>
2015-03-02 23:58:15 +0800

    Add SingleZooKeeperNodeMain

---
+++ levin-zookeeper-servers/src/main/java/org/levin/zookeeper/servers/single/SingleZooKeeperNodeMain.java
@@ -0,0 +1,9 @@
+package org.levin.zookeeper.servers.single;
+
+import org.apache.zookeeper.server.ZooKeeperServerMain;
+
+public class SingleZooKeeperNodeMain {
+    public static void main(String[] args) {
+        ZooKeeperServerMain.main(new String[] { "2811", "data" });
+    }
+}
