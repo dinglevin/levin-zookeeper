d3d4e66c9c18f10f6457fae5cc7c09c1b8485e2f
dinglevin <dingweiqiong@gmail.com>
2015-04-22 07:44:04 +0800

    update

--- levin-zookeeper-versionrepo/src/main/java/org/levin/zookeeper/versionrepo/ZooKeeperVersionRepo.java
+++ levin-zookeeper-versionrepo/src/main/java/org/levin/zookeeper/versionrepo/ZooKeeperVersionRepo.java
@@ -1,11 +1,30 @@
 package org.levin.zookeeper.versionrepo;
 
-import org.apache.zookeeper.ZooKeeper;
+import java.io.IOException;
 
-public class ZooKeeperVersionRepo {
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ZooKeeperVersionRepo implements Watcher {
+    private static final Logger logger = LoggerFactory.getLogger(ZooKeeperVersionRepo.class);
+    
+    private static final int DEFAULT_SESSION_TIMEOUT = 60000; // 60s
+    
     private final ZooKeeper zookeeper;
     
-    public ZooKeeperVersionRepo(String connectionString) {
-        zookeeper = new ZooKeeper(connectionString);
+    public ZooKeeperVersionRepo(String quorumServers) throws IOException {
+        this(quorumServers, DEFAULT_SESSION_TIMEOUT);
+    }
+    
+    public ZooKeeperVersionRepo(String quorumServers, int sessionTimeout) throws IOException {
+        zookeeper = new ZooKeeper(quorumServers, sessionTimeout, this);
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        logger.info("Received zookeer event: " + event);
     }
 }
